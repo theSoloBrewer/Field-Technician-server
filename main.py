ef1373f162ee3fb0d1b0353c6f9553e6fe376250
thesolobrewer <anarchos@loki.local>
2018-03-14 15:24:26 -0400

    new login UI

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created on Dec 12, 2017
 @author: theSoloBrewer
 '''
 from project import FTserver, db
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user
 from flask import send_from_directory, render_template, request, flash, redirect, url_for
 from models import User, Contact_Info
 
@@ -26,17 +26,17 @@ def register():
 	db.session.commit()
 	flash('User successfully registered')
 	return redirect(url_for('login'))
- 
-	
-	
+
+
+
 @FTserver.route('/login', methods=['GET','POST'])
 def login():
 	# Here we use a class of some kind to represent and validate our
 	# client-side form data. For example, WTForms is a library that will
 	# handle this for us, and we use a custom LoginForm to validate.
 	if request.method == 'GET':
-		return render_template('login.html')
-		
+		return render_template('login.html', active='login')
+
 	username = request.form['user']
 	password = request.form['pass']
 	registered_user = User.query.filter_by(username=username,password=password).first()
@@ -47,7 +47,10 @@ def login():
 	flash('Logged in successfully')
 	return redirect(request.args.get('next') or url_for('index'))
 
-
+@FTserver.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @FTserver.route('/static/<path:asset>/<page>')
@@ -65,7 +68,7 @@ def favicon():
 @FTserver.route('/index')
 def index():
 	return render_template('index.html', title='Home', active='home')
-   
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
 
 	FTserver.run(debug=True)
