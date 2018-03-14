09534cad315809d8db5ecd8da32ef1203486e6d6
thesolobrewer <computerzombie@msn.com>
2018-03-14 17:24:55 -0400

    modified main.py for separate routes

--- main.py
+++ main.py
@@ -3,72 +3,58 @@ Created on Dec 12, 2017
 
 @author: theSoloBrewer
 '''
-from project import FTserver, db
-from flask_login import LoginManager, login_user, logout_user
-from flask import send_from_directory, render_template, request, flash, redirect, url_for
-from models import User, Contact_Info
-
-login = LoginManager(FTserver)
+from project import FTserver
+import views
 
 
-@login.user_loader
-def load_user(u_id):
-	return User.query.get(int(u_id))
+import optparse
 
+def flaskrun(app, default_host="127.0.0.1",
+                  default_port="5000"):
+    """
+    Takes a flask.Flask instance and runs it. Parses
+    command-line flags to configure the app.
+    """
 
-@FTserver.route('/register' , methods=['GET','POST'])
-def register():
-	if request.method == 'GET':
-		return render_template('register.html', active='none')
-	cInfo = Contact_Info(request.form['fname'] + ' ' + request.form['lname'], request.form['email'], request.form['phone'])
-	user = User(request.form['username'] , request.form['password'], cInfo)
-	db.session.add(user)
-	db.session.commit()
-	flash('User successfully registered')
-	return redirect(url_for('login'))
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname of the Flask app " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the Flask app " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
 
+    # Two options useful for debugging purposes, but
+    # a bit dangerous so not exposed in the help message.
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+    parser.add_option("-p", "--profile",
+                      action="store_true", dest="profile",
+                      help=optparse.SUPPRESS_HELP)
 
+    options, _ = parser.parse_args()
 
-@FTserver.route('/login', methods=['GET','POST'])
-def login():
-	# Here we use a class of some kind to represent and validate our
-	# client-side form data. For example, WTForms is a library that will
-	# handle this for us, and we use a custom LoginForm to validate.
-	if request.method == 'GET':
-		return render_template('login.html', active='login')
+    # If the user selects the profiling option, then we need
+    # to do a little extra setup
+    if options.profile:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
 
-	username = request.form['user']
-	password = request.form['pass']
-	registered_user = User.query.filter_by(username=username,password=password).first()
-	if registered_user is None:
-		flash('Username or Password is invalid' , 'error')
-		return redirect(url_for('login'))
-	login_user(registered_user)
-	flash('Logged in successfully')
-	return redirect(request.args.get('next') or url_for('index'))
+        app.config['PROFILE'] = True
+        app.wsgi_app = ProfilerMiddleware(a4pp.wsgi_app,
+                       restrictions=[30])
+        options.debug = True
 
-@FTserver.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
 
-
-@FTserver.route('/static/<path:asset>/<page>')
-def send_static( asset, page):
-	print('asset')
-	return send_from_directory("/templates/static/" +asset, page, active=page, title=page)
-
-@FTserver.route('/<page>')
-def path(page):
-	return render_template(page + ".html", active=page, title=page)
-@FTserver.route('/favicon.ico')
-def favicon():
-	return ''
-@FTserver.route('/')
-@FTserver.route('/index')
-def index():
-	return render_template('index.html', title='Home', active='home')
 
 if __name__ == "__main__":
+	flaskrun(FTserver)
 
-	FTserver.run(debug=True)
